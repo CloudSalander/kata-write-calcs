7e05e221b348a179727e74e096fc74bc1000ec81
Cloud <ruben.as532@gmail.com>
2024-05-23 10:43:39 +0200

    Input and its validation

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+function isValidNumber(string $n): bool {
+    return is_numeric($n) && $n >= 0; 
+}
+
+$n = readline("Sobre qué número quieres calcular?");
+while(!(isValidNumber($n))) {
+    echo "Buen intento! Nah, en serio...".PHP_EOL;
+    $n = readline("Sobre qué número quieres calcular?");
+}
+
+
+?>
