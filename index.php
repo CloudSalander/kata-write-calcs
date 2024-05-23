a3aef86c2a60b5e9a68dd8d0cf449bf53c9f0200
Cloud <ruben.as532@gmail.com>
2024-05-23 10:52:13 +0200

    First line of file output added

--- index.php
+++ index.php
@@ -4,13 +4,21 @@ function isValidNumber(string $n): bool {
     return is_numeric($n) && $n >= 0; 
 }
 
+function getNewFile(string $n) {
+    $file = fopen("calculos_".$n.".txt",'w');
+    fwrite($file,"Número: ".$n.PHP_EOL);
+}
+
+
 $n = readline("Sobre qué número quieres calcular?");
 while(!(isValidNumber($n))) {
     echo "Buen intento! Nah, en serio...".PHP_EOL;
     $n = readline("Sobre qué número quieres calcular?");
 }
 
-$file = fopen("calculos_".$n.".txt",'w');
+$file = getNewFile($n);
+
+
 
 
 ?>
